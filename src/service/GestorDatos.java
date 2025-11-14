1e7d627c9200aa06d2c5b81fca62a4c6b77cdccc
Jony-English22 <jonathanrp200322@gmail.com>
2025-11-14 00:36:14 -0600

    Service

--- src/service/GestorDatos.java
+++ src/service/GestorDatos.java
@@ -1,6 +1,69 @@
 package service;
 
+import com.google.gson.Gson;
+import model.Participante;
+import model.Reto;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorDatos {
     private final String RUTA_DATOS = "data/datos.json";
     private Gson gson;
+
+    public GestorDatos() {
+        this.gson = new Gson();
+        crearArchivoSiNoExiste();
+    }
+    public static class DatosApp {
+        List<Participante> participantes = new ArrayList<>();
+        List<Reto> retos = new ArrayList<>();
+        int ultimoIdReto = 0;
+    }
+
+    public DatosApp cargarDatos() {
+        try {
+            String contenido = Files.readString(Paths.get(RUTA_DATOS));
+
+            if (contenido.isBlank()) {
+                return new DatosApp();
+            }
+
+            return gson.fromJson(contenido, DatosApp.class);
+
+        } catch (Exception e) {
+            System.out.println("Error al cargar datos: " + e.getMessage());
+            return new DatosApp();
+        }
+    }
+
+    public void guardarDatos(DatosApp datos) {
+        try (FileWriter writer = new FileWriter(RUTA_DATOS)) {
+            gson.toJson(datos, writer);
+        } catch (IOException e) {
+            System.out.println("Error al guardar datos: " + e.getMessage());
+        }
+    }
+
+    private void crearArchivoSiNoExiste() {
+        try {
+            File archivo = new File(RUTA_DATOS);
+
+            if (!archivo.exists()) {
+                archivo.getParentFile().mkdirs();
+                archivo.createNewFile();
+
+                guardarDatos(new DatosApp());
+
+                System.out.println("Archivo datos.json creado");
+            }
+        } catch (IOException e) {
+            System.out.println("Error creando archivo: " + e.getMessage());
+        }
+    }
 }
--- src/service/GestorParticipantes.java
+++ src/service/GestorParticipantes.java
@@ -4,9 +4,47 @@ import model.Participante;
 import model.Reto;
 
 import java.util.List;
+import java.util.Map;
 
 public class GestorParticipantes {
     private List<Participante> participantes;
     private List<Reto>         retos;
     private int                ultimoIdReto;
+
+    public boolean registrarParticipante(String nombre) {
+
+        return false;
+    }
+
+    public boolean eliminarParticipante(String nombre) {
+        return true;
+    }
+
+    public Participante obtenerParticipante(String nombre) {
+        return ;
+    }
+
+    public List<Participante> listarParticipantes() {
+     return ;
+    }
+
+    public int calcularPuntosTotales(String nombre) {
+        return ;
+    }
+
+    public boolean existeParticipantes(String nombre) {
+        return ;
+    }
+
+    public List<Map.Entry<Participante, Integer>> obtenerRankingOrdenado() {
+
+    }
+
+    public void cargarParticipantes(List<Participante> participantes) {
+
+    }
+
+    public List<Participante> getParticipantes() {
+
+    }
 }
